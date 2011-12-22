e14e2b3f3fd2c4740319dc79f411f8c0013e44cc
じゅんいち☆かとう <j5ik2o@gmail.com>
2011-12-22 14:08:32 +0900

    new file

---
+++ project/Build.scala
@@ -0,0 +1,45 @@
+import sbt._
+import Keys._
+
+object DDDBaseBuild extends Build {
+
+   //val specs2Test = "org.specs2" %% "specs2" % "1.5" % "test"
+   val scalaTest = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+   val junit = "junit" % "junit" % "4.8.1" % "test" 
+   val mockito = "org.mockito" % "mockito-core" % "1.8.5" % "test"
+
+   lazy val commonSettings = Defaults.defaultSettings ++ Seq(
+      organization := "org.sisioh",
+      scalaVersion := "2.9.1",
+      libraryDependencies ++= Seq(junit,mockito,scalaTest),
+      scalacOptions ++= Seq("-unchecked", "-deprecation"),
+      shellPrompt := { "sbt (%s)> " format projectId(_) },
+      publish
+      )
+  val publish = publishTo <<= (version) { version: String =>
+    if (version.trim.endsWith("SNAPSHOT")) {
+      Some(Resolver.ssh("sisioh-repo-snapshot", 
+	    "maven.sisioh.org", "/var/www/maven.sisioh.org/snapshot"))
+    }else{
+      Some(Resolver.ssh("sisioh-repo-release", 
+            "maven.sisioh.org", "/var/www/maven.sisioh.org/release"))
+    }
+  }
+
+    lazy val root:Project = Project("scala-dddbase",
+                            file("."),
+                            settings = commonSettings,
+			    aggregate = Seq(core, spec))
+
+    val core:Project = Project("scala-dddbase-core",
+                       file("scala-dddbase-core"),
+                       settings = commonSettings)
+
+    val spec:Project = Project("scala-dddbase-spec",
+                       file("scala-dddbase-spec"),
+                       settings = commonSettings)
+
+  def projectId(state: State) = extracted(state).currentProject.id
+  def extracted(state: State) = Project extract state
+
+}
---
+++ project/plugins/build.sbt
@@ -0,0 +1,7 @@
+resolvers += Classpaths.typesafeResolver
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse" % "1.5.0")
+
+resolvers += "less is" at "http://repo.lessis.me"
+
+addSbtPlugin("me.lessis" % "sbt-growl-plugin" % "0.1.3")
