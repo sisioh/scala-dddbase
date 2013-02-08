69c6072eea09abf224c9c8a6ed0859c870bf3c45
mtgto <hogerappa@gmail.com>
2013-02-09 00:59:13 +0900

    Scala 2.10とのクロスコンパイルの追加と関連ライブラリのアップデート。
    - Scala 2.10とのクロスコンパイルを追加
    - いくつかのライブラリを最新版へアップデート
     - scalaz-coreを6.0.3から6.0.4へ(Scala 2.10で使用するため)
     - scalatestを1.6.1から1.9.1へ
     - mockitoを1.8.5から1.9.5へ
     - sbt versionを0.11.2から0.12.2へ
     - sbteclipseを2.0.0-RC1から2.1.1へ
    - sbtプラグインの置き方を今風にした

--- project/Build.scala
+++ project/Build.scala
@@ -4,21 +4,22 @@ import Keys._
 object DDDBaseBuild extends Build {
 
    //val specs2Test = "org.specs2" %% "specs2" % "1.5" % "test"
-   val scalaTest = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+   val scalaTest = "org.scalatest" %% "scalatest" % "1.9.1" % "test"
    val junit = "junit" % "junit" % "4.8.1" % "test"
-   val mockito = "org.mockito" % "mockito-core" % "1.8.5" % "test"
+   val mockito = "org.mockito" % "mockito-core" % "1.9.5" % "test"
 
    ///val = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
 
   // NOTE: New Group ID from Scalaz 6.x and onwards
-   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.3"
+   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
 
 
    lazy val commonSettings = Defaults.defaultSettings ++ Seq(
       organization := "org.sisioh",
       version := "0.0.1",
       scalaVersion := "2.9.1",
-      sbtVersion := "0.11.2",
+      crossScalaVersions := Seq("2.9.1", "2.10.0"),
+      sbtVersion := "0.12.2",
       libraryDependencies ++= Seq(junit,mockito,scalaTest,scalaz),
       scalacOptions ++= Seq("-unchecked", "-deprecation"),
       shellPrompt := { "sbt (%s)> " format projectId(_) },
