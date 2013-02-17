684cb85937bd4dacbb6f65aa5bc8e074a886e4d6
Junichi Kato <j5ik2o@gmail.com>
2013-02-17 21:08:51 +0900

    - IDEAでのビルドを考慮してscalaVersionを2.9.2に変更した - crossScalaVersionsは2.9.1, 2.9.2, 2.10.0とした

--- project/Build.scala
+++ project/Build.scala
@@ -17,8 +17,8 @@ object DDDBaseBuild extends Build {
    lazy val commonSettings = Defaults.defaultSettings ++ Seq(
       organization := "org.sisioh",
       version := "0.0.1",
-      scalaVersion := "2.9.1",
-      crossScalaVersions := Seq("2.9.1", "2.10.0"),
+      scalaVersion := "2.9.2",
+      crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.0"),
       sbtVersion := "0.12.2",
       libraryDependencies ++= Seq(junit,mockito,scalaTest,scalaz),
       scalacOptions ++= Seq("-unchecked", "-deprecation"),
