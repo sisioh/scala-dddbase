c2c053049b3f9b4b80217d70c1908cf055974ea3
Junichi Kato <j5ik2o@gmail.com>
2013-04-21 18:28:15 +0900

    - IdentityImplのケースクラス廃止 - Scaladoc追加

--- scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/Identity.scala
+++ scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/Identity.scala
@@ -1,15 +1,55 @@
 package org.sisioh.dddbase.core
 
+/**
+ * エンティティの識別子を表すトレイト。
+ *
+ * エンティティで用いられる識別子を型として表現することを目的としている。
+ *
+ * @tparam A 識別子の値を表す型
+ */
 trait Identity[A] {
+  /**
+   * 識別子の値を取得する。
+   *
+   * @return 識別子の値
+   */
   def value: A
 }
 
-private case class IdentityImpl[A](value: A) extends Identity[A]
+private class IdentityImpl[A](val value: A) extends Identity[A] with Serializable {
 
+  override def toString = s"Identity($value)"
+
+  override def equals(obj: Any) = obj match {
+    case that: Identity[_] =>
+      value == that.value
+    case _ => false
+  }
+
+  override def hashCode = 31 * value.##
+}
+
+/**
+ * コンパニオンオブジェクト。
+ */
 object Identity {
 
-  def apply[A](value: => A): Identity[A] = IdentityImpl(value)
+  /**
+   * [[org.sisioh.dddbase.core.Identity]]を生成する。
+   *
+   * @param value 識別子の値
+   * @tparam A 識別子の値の型
+   * @return [[org.sisioh.dddbase.core.Identity]]
+   */
+  def apply[A](value: => A): Identity[A] = new IdentityImpl(value)
 
+  /**
+   * 抽出子メソッド。
+   *
+   * @param v [[org.sisioh.dddbase.core.Identity]]
+   * @tparam A 識別子の値の型
+   * @return 識別子の値
+   */
   def unapply[A](v: Identity[A]): Option[A] = Some(v.value)
 
 }
