88658e4dfdac1db298bd9e7547ebe3084102bdad
Junichi Kato <j5ik2o@gmail.com>
2013-04-24 09:46:48 +0900

    refactor

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sbt clean publish
 rm -fr /tmp/repos
 mv ./repos /tmp
@@ -6,3 +8,4 @@ git checkout gh-pages
 git add repos
 git commit -m "$1"
 git push origin gh-pages
+git checkout develop
