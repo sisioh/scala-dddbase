1c1293a1d1a7f0daacfbd0fd76491225ac5b6071
Junichi Kato <j5ik2o@gmail.com>
2013-06-20 20:16:55 +0900

    不要インポート削除

--- scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/Repository.scala
+++ scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/Repository.scala
@@ -17,7 +17,6 @@
 package org.sisioh.dddbase.core
 
 import scala.util._
-import org.sisioh.dddbase.spec.Specification
 import scala.collection.mutable.ListBuffer
 
 /**
@@ -268,28 +267,7 @@ trait EntityReaderByPredicate[ID <: Identity[_], T <: Entity[ID]] {
 
 }
 
-/**
- * [[org.sisioh.dddbase.spec.Specification]]を述語に取り、エンティティを検索することができるトレイト。
- *
- * @tparam ID 識別子の型
- * @tparam T エンティティの型
- */
-trait EntityReaderBySpecification[ID <: Identity[_], T <: Entity[ID]] {
-  this: EntityReader[ID, T] =>
 
-  /**
-   * [[org.sisioh.dddbase.spec.Specification]]に該当したエンティティを取得する。
-   *
-   * @param specification [[org.sisioh.dddbase.spec.Specification]]
-   * @return Success:
-   *         チャンク
-   *         Failure:
-   *         EntityNotFoundExceptionは、エンティティが見つからなかった場合
-   *         RepositoryExceptionは、リポジトリにアクセスできなかった場合。
-   */
-  def filterBySpecification(specification: Specification[T], index: Option[Int] = None, maxEntities: Option[Int] = None): Try[EntitiesChunk[ID, T]]
-
-}
 
 /**
  * ページングによる検索を行うためのトレイト。
