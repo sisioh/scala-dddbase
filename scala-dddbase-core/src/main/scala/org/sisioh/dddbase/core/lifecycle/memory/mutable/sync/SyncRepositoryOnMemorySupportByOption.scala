a34d1efbd35b7a7a0e3529a8fb10d7b747070bfa
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-15 16:32:32 +0900

    reformat

--- scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/lifecycle/memory/mutable/sync/SyncRepositoryOnMemorySupportByOption.scala
+++ scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/lifecycle/memory/mutable/sync/SyncRepositoryOnMemorySupportByOption.scala
@@ -6,24 +6,24 @@ import org.sisioh.dddbase.core.model._
 import scala.util._
 
 /**
-  * [[org.sisioh.dddbase.core.lifecycle.memory.mutable.sync.SyncRepositoryOnMemorySupport]]にOption型のサポートを追加するトレイト。
-  *
-  * @tparam R 当該リポジトリを実装する派生型
-  * @tparam ID エンティティの識別子の型
-  * @tparam T エンティティの型
-  */
+ * [[org.sisioh.dddbase.core.lifecycle.memory.mutable.sync.SyncRepositoryOnMemorySupport]]にOption型のサポートを追加するトレイト。
+ *
+ * @tparam R 当該リポジトリを実装する派生型
+ * @tparam ID エンティティの識別子の型
+ * @tparam T エンティティの型
+ */
 trait SyncRepositoryOnMemorySupportByOption
 [+R <: SyncRepository[_, ID, T],
- ID <: Identity[_],
- T <: Entity[ID] with EntityCloneable[ID, T] with Ordered[T]]
+ID <: Identity[_],
+T <: Entity[ID] with EntityCloneable[ID, T] with Ordered[T]]
   extends SyncRepositoryOnMemorySupport[R, ID, T]
-     with SyncEntityReaderByOption[ID, T] {
+  with SyncEntityReaderByOption[ID, T] {
 
-     def resolveOption(identity: ID): Try[Option[T]] = synchronized {
-       resolve(identity).map(Some(_)).recoverWith {
-         case ex: EntityNotFoundException =>
-           Success(None)
-       }
-     }
+  def resolveOption(identity: ID): Try[Option[T]] = synchronized {
+    resolve(identity).map(Some(_)).recoverWith {
+      case ex: EntityNotFoundException =>
+        Success(None)
+    }
+  }
 
-   }
+}
