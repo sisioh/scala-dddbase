e07dc8494f13a6b52b9240d4f4816930d0061008
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-15 16:32:14 +0900

    scaladoc

--- scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/lifecycle/EntityWriter.scala
+++ scala-dddbase-core/src/main/scala/org/sisioh/dddbase/core/lifecycle/EntityWriter.scala
@@ -21,7 +21,7 @@ import org.sisioh.dddbase.core.model.{Entity, Identity}
 /**
  * [[org.sisioh.dddbase.core.model.Identity]]を用いて
  * [[org.sisioh.dddbase.core.model.Entity]]
- * を書き込むための責務を表すインターフェイス。
+ * を書き込むための責務を表すトレイト。
  *
  * @tparam ID 識別子の型
  * @tparam T エンティティの型
