56406aeb02490d2f686b46b8dcf80559e72a7ba2
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-19 11:57:29 +0900

    Scaladoc

--- scala-dddbase-event/src/main/scala/org/sisioh/dddbase/event/lifecycle/EventType.scala
+++ scala-dddbase-event/src/main/scala/org/sisioh/dddbase/event/lifecycle/EventType.scala
@@ -4,5 +4,21 @@ package org.sisioh.dddbase.event.lifecycle
  * イベントタイプ。
  */
 object EventType extends Enumeration {
-  val Resolve, Store, Delete = Value
+
+  /**
+   * 参照
+   */
+  val Resolve,
+
+  /**
+   * 保存　
+   */
+  Store,
+
+
+  /**
+   * 削除
+   */
+  Delete = Value
+
 }
