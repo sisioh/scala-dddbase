41b8e4e29a0f713a70e1df6155f79bf9e8945b48
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-29 01:37:19 +0900

    scaladoc & test

--- scala-dddbase-event/src/main/scala/org/sisioh/dddbase/event/lifecycle/AsyncRepositoryEventSupport.scala
+++ scala-dddbase-event/src/main/scala/org/sisioh/dddbase/event/lifecycle/AsyncRepositoryEventSupport.scala
@@ -6,21 +6,44 @@ import org.sisioh.dddbase.event.async.AsyncDomainEventSubscriber
 import org.sisioh.dddbase.event.mutable.async.GenericAsyncDomainEventPublisher
 import scala.concurrent.Future
 
+/**
+ * 非同期型リポジトリにイベント管理機能を追加するためのトレイト。
+ *
+ * @tparam ID エンティティの識別子の型
+ * @tparam E エンティティの型
+ */
 trait AsyncRepositoryEventSupport[ID <: Identity[_], E <: Entity[ID]]
   extends AsyncRepository[ID, E] {
 
   private val eventPublisher = GenericAsyncDomainEventPublisher[EntityIOEvent[ID, E]]()
 
+  /**
+   * [[org.sisioh.dddbase.event.async.AsyncDomainEventSubscriber]]を登録する。
+   *
+   * @param subscriber [[org.sisioh.dddbase.event.async.AsyncDomainEventSubscriber]]
+   */
   def subscribe(subscriber: AsyncDomainEventSubscriber[EntityIOEvent[ID, E], Unit]): This = {
     eventPublisher.subscribe(subscriber)
     this.asInstanceOf[This]
   }
 
+  /**
+   * [[org.sisioh.dddbase.event.async.AsyncDomainEventPublisher]]を削除する。
+   *
+   * @param subscriber [[org.sisioh.dddbase.event.async.AsyncDomainEventPublisher]]
+   */
   def unsubscribe(subscriber: AsyncDomainEventSubscriber[EntityIOEvent[ID, E], Unit]): This = {
     eventPublisher.unsubscribe(subscriber)
     this.asInstanceOf[This]
   }
 
+  /**
+   * [[org.sisioh.dddbase.event.lifecycle.EntityIOEvent]]を生成する。
+   *
+   * @param entity エンティティ
+   * @param eventType イベントの種類
+   * @return [[org.sisioh.dddbase.event.lifecycle.EntityIOEvent]]
+   */
   protected def createEntityIOEvent(entity: E, eventType: EventType.Value): EntityIOEvent[ID, E]
 
   abstract override def store(entity: E): Future[AsyncResultWithEntity[This, ID, E]] = {
